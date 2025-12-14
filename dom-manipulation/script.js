57747f8e94023728fa6383a555b76de9309c7e8c
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-14 11:03:33 +0000

    added a js file

---
+++ dom-manipulation/script.js
@@ -0,0 +1,6 @@
+document. addEventListener('DOMContentLoaded',() => {
+    const FirstQuote = document.getElementById('First Quote');
+    const SecondQuote = document.getElementById('Second Quote');
+    const addQuote = document.getElementById('addQuote');
+    const showQuote = document.getElementById('showQuote');
+})
