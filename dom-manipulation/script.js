8eee1017bae6ec570e92e9fdc8fda78dd7a22918
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-18 13:48:28 +0000

    added a js import export button

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -74,4 +74,16 @@ const addButton = document.createElement('button');
  
  document.body.appendChild(form);
 createAddQuoteForm();
+
+ function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
+  
 });
