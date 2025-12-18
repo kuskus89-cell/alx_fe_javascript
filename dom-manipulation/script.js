22f7bd1969ae30381cc2ba3cfc58e9790916c8bf
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-18 12:50:42 +0000

    Made some changes in the code

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,48 +4,50 @@ document. addEventListener('DOMContentLoaded',() => {
  
  const quotes = [
        {
-
-       text:"Sometimes we fail only to come back stronger.Remember failing is a mistake but staying down is an option" 
+       text:"Sometimes we fail only to come back stronger.Remember failing is a mistake but staying down is an option." 
        category: "Motivation "
-
         }
     ];
 
    function showRandomQuote() {
      const randomIndex = Math.floor(Math.random() * quotes.length);
      const randomQuote = quotes[randomIndex];
-    quoteDisplay.textContent = randomQuote.text;
+    quoteDisplay.textContent = quotes[randomIndex].text;
    }
 
-   showRandomQuote();
-
  newQuoteBtn.addEventListener('click', showRandomQuote);
+ showRandomQuote();
 
  function createAddQuoteForm(){
   const form = document.createElement('form');
 
 const quoteInput = document.createElement('input');
- quoteInput.placeholder = 'Enter Quote Text';
+ quoteInput.placeholder = 'Enter quote text';
 
  const categoryInput = document.createElement('input');
  categoryInput.placeholder = 'Enter  quote category';
 
 const addButton = document.createElement('button');
- addButton.textContent = 'Add Quote';
+ addButton.type = 'submit';
 
- form.appendChild('quoteInput');
- form.appendChild('categoryInput');
- form.appendChild('addButton');
-
- document.body.appendChild(form);
+ form.appendChild(quoteInput);
+ form.appendChild(categoryInput);
+ form.appendChild(addButton);
  }
 
- form.addEventListener('submit', function (Enter){
-  enter.preventDefault();
+ form.addEventListener('submit', (e) =>{
+  e.preventDefault();
 
   quotes.push({
     text:quoteInput.value,
     category:categoryInput.value
-  })
- })
+  });
+  quoteInput.value = '';
+  categoryInput.value = '';
+
+  showRandomQuote();
+ }); 
+ 
+ document.body.appendChild(form);
+createAddQuoteForm();
 });
